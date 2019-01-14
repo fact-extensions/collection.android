ba97a40719e66099480666c1adec8a209af672b5
Malachi Burke <malachi.burke@gmail.com>
2019-01-13 19:38:27 -0800

    Upgrading nuget version management

--- src/setenv.sh
+++ src/setenv.sh
@@ -2,7 +2,8 @@
 
 . ../ext/useful-scripts/setenv
 . ../ext/myget/setenv_nuget.sh
+. ../ext/useful-scripts/scm/setenv-version.sh $PWD
 
-export VERSION_PREFIX=1.0.0
-export VERSION_SUFFIX=alpha011
-export VERSION=$VERSION_PREFIX-$VERSION_SUFFIX
+#export VERSION_PREFIX=1.0.0
+#export VERSION_SUFFIX=alpha011
+#export VERSION=$VERSION_PREFIX-$VERSION_SUFFIX
