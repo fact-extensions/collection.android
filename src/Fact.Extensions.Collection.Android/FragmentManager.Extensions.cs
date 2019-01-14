fd73e9d64b14892ab65f6d8753502a1b435bef96
Malachi Burke <malachi.burke@gmail.com>
2019-01-13 19:22:15 -0800

    Adding v4 compat fragment support
    Not specifically tested, but confident it will work

--- src/Fact.Extensions.Collection.Android/FragmentManager.Extensions.cs
+++ src/Fact.Extensions.Collection.Android/FragmentManager.Extensions.cs
@@ -31,5 +31,31 @@ namespace Fact.Extensions.Collection
             Show(fragmentManager, containerViewId, fragment);
             return fragment;
         }
+
+        /// <summary>
+        /// Calls BeginTransaction, then calls tx.Replace then tx.Commit
+        /// </summary>
+        /// <param name="fragmentManager"></param>
+        public static void Show(this Android.Support.V4.App.FragmentManager fragmentManager, 
+            int containerViewId,
+            Android.Support.V4.App.Fragment fragment)
+        {
+            Android.Support.V4.App.FragmentTransaction tx = fragmentManager.BeginTransaction();
+            tx.Replace(containerViewId, fragment);
+            tx.Commit();
+        }
+
+
+        /// <summary>
+        /// Calls BeginTransaction, then calls tx.Replace then tx.Commit
+        /// </summary>
+        /// <param name="fragmentManager"></param>
+        public static T Show<T>(this Android.Support.V4.App.FragmentManager fragmentManager, int containerViewId)
+            where T : Android.Support.V4.App.Fragment, new()
+        {
+            var fragment = new T();
+            Show(fragmentManager, containerViewId, fragment);
+            return fragment;
+        }
     }
 }
